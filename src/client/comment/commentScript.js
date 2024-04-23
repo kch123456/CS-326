d0ac6606d00bc5c6caa1e3703153fa4c528e4edb
Kevin Chen <Kunhongchen@umass.edu>
2024-04-23 19:59:49 -0400

    adding stuff

--- src/client/comment/commentScript.js
+++ src/client/comment/commentScript.js
@@ -1,8 +1,14 @@
 let testingTask = {"name":"milestone-02","dueDate": new Date(4/25/2024),"status":"in progress","description":"annoying and long"};
 
+
+function show(){
 let name = testingTask.name;
 let date = testingTask.dueDate;
 let status = testingTask.status;
 let description = testingTask.description;
-
-document.getElementById('')
+document.getElementById('taskName').innerHTML = name;
+document.getElementById('dueDate').innerHTML = date;
+document.getElementById('status').innerHTML = status;
+document.getElementById('description').innerHTML = description;
+}
+show();
