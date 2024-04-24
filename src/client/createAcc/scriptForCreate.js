199687948ee67400487b311d0f4b023e0d16d966
malvi07 <150569058+malvi07@users.noreply.github.com>
2024-04-24 14:20:14 -0400

    Updated create acc page

--- src/client/createAcc/scriptForCreate.js
+++ src/client/createAcc/scriptForCreate.js
@@ -1,20 +1,30 @@
-import db from "/src/client/server/db.js";
+// import db from "/src/client/server/db.js";
 
-async function createAcc() {
-    let userName = document.getElementById('UserName').value;
-    let password = document.getElementById('password').value;
+// async function createAcc() {
+//     let userName = document.getElementById('UserName').value;
+//     let password = document.getElementById('password').value;
 
-    const user = { 'userName': userName, 'password': password };
+//     const user = { 'userName': userName, 'password': password };
 
-    try {
-        const response = await db.put(user);
-        console.log('Document added successfully:', response);
-    } catch (error) {
-        console.error('Error adding document:', error);
-    }
-}
+//     try {
+//         const response = await db.put(user);
+//         console.log('Document added successfully:', response);
+//     } catch (error) {
+//         console.error('Error adding document:', error);
+//     }
+// }
 
-document.getElementById('register').addEventListener('click',async function(){
-    await createAcc();
-    return;
+// document.getElementById('register').addEventListener('click',async function(){
+//     await createAcc();
+//     return;
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let registerButton = document.getElementById("register");
+
+    registerButton.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        window.location.href = "/login/login.html";
+    });
 });
+
