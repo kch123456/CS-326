52a5449b899ca121b027b034b19dcf042586943f
malvi07 <150569058+malvi07@users.noreply.github.com>
2024-04-24 11:15:14 -0400

    Created a logout button and page.

--- src/client/homepage/homepage.js
+++ src/client/homepage/homepage.js
@@ -1,13 +1,32 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     let continueButton = document.createElement('button');
+//     continueButton.textContent = 'Continue to Dashboard';
+//     continueButton.id = 'continueButton';
+//     continueButton.style.marginTop = '20px';
+//     document.getElementById('introduction').appendChild(continueButton);
+ 
+ 
+//     continueButton.addEventListener('click', function() {
+//         window.location.href = '/dashboard/dashboard.html';
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
     let continueButton = document.createElement('button');
     continueButton.textContent = 'Continue to Dashboard';
     continueButton.id = 'continueButton';
     continueButton.style.marginTop = '20px';
     document.getElementById('introduction').appendChild(continueButton);
- 
- 
+
     continueButton.addEventListener('click', function() {
-        window.location.href = '/views/dashboard.html';
+        window.location.href = '/dashboard/dashboard.html';
     });
- });
+
+    // Logout button event listener
+    document.getElementById('logoutButton').addEventListener('click', function() {
+        window.location.href = '/logout/logout.html';
+    });
+});
+
+
  
