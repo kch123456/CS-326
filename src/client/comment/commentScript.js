866aed712fdc92139dfb46ee9408baf22afb3167
Kevin Chen <Kunhongchen@umass.edu>
2024-04-23 21:33:31 -0400

    adding stuff to comment

--- src/client/comment/commentScript.js
+++ src/client/comment/commentScript.js
@@ -1,7 +1,7 @@
-let testingTask = {"name":"milestone-02","dueDate": new Date(4/25/2024),"status":"in progress","description":"annoying and long"};
+let testingTask = {"name":"milestone-02","dueDate": new Date("4/25/2024"),"status":"in progress","description":"annoying and long"};
 
 
-function show(){
+ function show(){
 let name = testingTask.name;
 let date = testingTask.dueDate;
 let status = testingTask.status;
@@ -11,4 +11,16 @@ document.getElementById('dueDate').innerHTML = date;
 document.getElementById('status').innerHTML = status;
 document.getElementById('description').innerHTML = description;
 }
+function addToDiscussion(){
+let discussion = document.querySelector('.discussion');
+let message = document.getElementById('message').value;
+let section = document.createElement('p');
+section.classList.add('subWrapper');
+section.innerHTML = message;
+discussion.appendChild(section);
+}
+document.getElementById('enter').addEventListener('click', function(){
+    addToDiscussion();
+    return;
+});
 show();
