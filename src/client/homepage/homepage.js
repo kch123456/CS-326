8daf1b65a4945357777e7517b0b2483ffa251aa5
malvi07 <150569058+malvi07@users.noreply.github.com>
2024-04-24 21:41:59 -0400

    Update homepage.js

--- src/client/homepage/homepage.js
+++ src/client/homepage/homepage.js
@@ -1,16 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     let continueButton = document.createElement('button');
-//     continueButton.textContent = 'Continue to Dashboard';
-//     continueButton.id = 'continueButton';
-//     continueButton.style.marginTop = '20px';
-//     document.getElementById('introduction').appendChild(continueButton);
- 
- 
-//     continueButton.addEventListener('click', function() {
-//         window.location.href = '/dashboard/dashboard.html';
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     let continueButton = document.createElement('button');
     continueButton.textContent = 'Continue to Dashboard';
